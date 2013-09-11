5eff7cbc2b025bddfaef36b464bee677fdb710f3
Samantha Carow <samantha.carow@gmail.com>
2013-09-10 18:40:15 -0700

    fixed errors

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,40 +1,44 @@
 get '/' do
-  if @user #won't trigger if user=nil
-    session[:user_id] = @user.id
-    erb :index
-  elsif @user = User.new
-    @user.login_error
-    redirect '/users/new'
-  end
+
+  @users = User.all
+
+  erb :index
 end
 
 #----------- SESSIONS -----------
 
 get '/sessions/new' do
-   
+
   erb :sign_in
 end
 
 post '/sessions' do
-  @user = User.authenticate(params)
+  user = User.authenticate(params[:email], params[:password])
+
+  if user
+    session[:user_id] = user.id
+  end
 
   redirect '/'
 end
 
 delete '/sessions/:id' do
   session.clear
-  redirect '/'
 end
 
 #----------- USERS -----------
 
 get '/users/new' do
-  
+
   erb :sign_up
 end
 
 post '/users' do
-  @user = User.new(params[:user]) 
-  redirect '/' if @user.save
-  erb :sign_up
+  user = User.new(name: params[:user][:name], email: params[:user][:email]) 
+  user.password= params[:user][:password]
+  user.save!
+  
+  session[:user_id]= user.id  
+  
+  redirect '/'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,24 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
 
-def self.authenticate(login)
-  user = self.find_by_name(login[:name])
-  return nil if user.nil?
-  user.password == login[:password] ? user : nil
-end
+  def self.authenticate(email, password)
+    user = self.find_by_email(email)
+    return nil if user.nil?
+    user.password == password ? user : nil
+  end
 
-def login_error
-  self.errors.add(:name_or_password, "is not recognized")
-end
+  def login_error
+    self.errors.add(:name_or_password, "is not recognized")
+  end
 
-def password
-  @password ||= Password.new(password_hash)
-end
+  def password
+    @password ||= Password.new(password_hash)
+  end
 
-def password=(new_password)
-  @password = Password.create(new_password)
-  self.password_hash = @password
-end
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 
 
 end
